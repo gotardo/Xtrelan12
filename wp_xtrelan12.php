8a912b68c8a591a52feebd9209f44423cc8d866e
gotardo <contacto@gotardo.es>
2012-09-27 20:33:28 +0200

    Crear páginas en el menú

--- wp_xtrelan12.php
+++ wp_xtrelan12.php
@@ -4,19 +4,57 @@
       Plugin Name: WP Xtrelan12
       Plugin URI: http://blog.gotardo.es/xtrelan-2012
       Description: Plugin para el taller de desarrollo de plugins de Xtrelan 2012 (27/09/2012)
-      Version: 0.1
+      Version: 0.2
       Author: Gotardo González (@gotardo)
       Author URI: http://blog.gotardo.es
    */
 
 
-    //Función principal del plugin
-    function wp_xtrelan12 () {
-        //Añadimos acción a admin_notices, se ejecutará siempre
-        add_action( 'admin_notices', function () { echo '<div class="updated"><p>Hola, mundo</p></div>';} );
+    //clase principal del plugin
+
+    class xtrelan12 {
+
+        //Constructor
+        function xtrelan12 () {
+            //Registrar acciones
+            add_action('admin_menu', array($this,'config_menu_page'));
+        }
+
+        //Añadir menús
+        function config_menu_page () {
+            //Añadir una men
+            add_menu_page("Xtrelan 2012 | config", "Xtrelan 2012", 6, 'xtrelan12', array($this,'admin_page'));
+            add_submenu_page("xtrelan12", "Info", "Info", 6, "xtr-submenu1", array($this, 'info_page'));
+            add_submenu_page("xtrelan12", "Info", "Info", 6, "xtr-submenu1", array($this, 'info_page'));
+        }
+
+        function info_page () {
+            echo "<h1>Xtrelan 2012</h1>
+                    <h2>¡Esta es la página de administración!</h2>
+                    <div>
+                        <ul>
+                            <li>Nombre:  "  . get_bloginfo("name") . "</li>
+                            <li>Descripción:  "  . get_bloginfo("description") . "</li>
+                            <li>Admin:  "   . get_bloginfo("admin_email") . "</li>
+                            <li>Template:  "   . get_bloginfo("template_url") . "</li>
+
+                            <li>Fichero: " . __FILE__ . "</li>
+                        </ul>
+                    </div>
+                    ";
+        }
+
+        function admin_page() {
+            echo "<h1>Xtrelan 2012</h1>
+                    <h2>¡Esta es la página de administración!</h2>
+            ";
+        }
     }
 
 
+
     //Llamamos a la función principal del plugin
-    wp_xtrelan12 ();
+    $xtr = new xtrelan12();
+
+
 ?>
